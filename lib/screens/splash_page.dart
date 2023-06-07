4edf50a97bd9ca2e2f66b2a4af07bb9d8078ee42
91972 <KevalKothiya>
2023-06-07 18:20:51 +0530

    07-06-2023

--- lib/screens/splash_page.dart
+++ lib/screens/splash_page.dart
@@ -1,12 +1,8 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:sizer/sizer.dart';
 import 'package:universe/controller/controller_provider.dart';
-import 'package:universe/model/utils.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -15,9 +11,9 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
   List<AnimationController> controller = [];
-
 
   @override
   void initState() {
@@ -46,59 +42,19 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
   loaddata() async {
     await Provider.of<loadData_Provider>(context, listen: false).loadData();
   }
+
   @override
   Widget build(BuildContext context) {
-    // Timer(
-    //   Duration(seconds: 2),
-    //   () {
-    //     Navigator.restorablePushReplacementNamed(context, '/');
-    //   },
-    // );
-    Future.delayed(Duration(seconds: 2))
+    Future.delayed(Duration(seconds: 10))
         .then((value) => Navigator.of(context).pushReplacementNamed('/'));
     return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.black,
       child: Container(
-        child: Column(
-          children: [
-            Text(
-              "KK",
-              style: GoogleFonts.rampartOne(
-                fontSize: 50,
-                color: CupertinoColors.white,
-                decoration: TextDecoration.none,
-              ),
-            ),
-            Text(
-              "2023 KK, lnc. All right reserved.",
-              style: TextThemes.textStyleDark,
-            ),
-            Text("Starting..", style: TextThemes.textStyleDark),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 6.h,
-                    width: 12.w,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "assests/splash_screen_image/unreal.jpg",
-                        ),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                      "Unreal Engine CopyRight 1998 - 2023, Epic Games, lnc. All rights reserved.",
-                      style: TextThemes.textStyleDark),
-                  flex: 6,
-                ),
-              ],
-            ),
-          ],
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assests/splash_screen_image/d-1.jpg"),
+            fit: BoxFit.cover
+          ),
         ),
       ),
     );
